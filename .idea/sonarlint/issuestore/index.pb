1899d186c2936426dd6ae17db3e17502ef6ed670
Debashis Paul <send2deb@gmail.com>
2018-03-28 00:41:47 +0100

    Complete exercise TFragments.05-Exercise-InterfaceCommunication

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+>
+app/.gitignore,5/1/51e1c5d383dfaa35e0e7e5873a0a99355a86880f
+z
+Japp/src/main/java/com/example/android/android_me/ui/MasterListAdapter.java,1/d/1dbb56d43d1f3589404463e795f655e7fcac32c6
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+z
+Japp/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java,8/1/81328eab40614a81aff12ede800510c5f2545ee3
+`
+0app/src/main/res/layout/fragment_master_list.xml,8/3/838fff326388ffb3ba86fb6045a036535a59a93b
+Y
+)app/src/main/res/layout/activity_main.xml,7/6/76b07e40f24cce093536731477540f12e7da3cb6
+{
+Kapp/src/main/java/com/example/android/android_me/ui/MasterListFragment.java,a/e/aea3421f23c4a9fa4ea7224ca0351d4f33041658
+u
+Eapp/src/main/java/com/example/android/android_me/ui/MainActivity.java,d/8/d80bd7c9d82a41b2b594254366d949e4ff5eecf5
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -16,27 +16,44 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
 
+
 // This fragment displays all of the AndroidMe images in one large list
 // The list appears as a grid of images
 public class MasterListFragment extends Fragment {
 
-    // TODO (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
+    OnImageClickListener mCallback;
+    // Completed (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
         // The callback is a method named onImageSelected(int position) that contains information about
         // which position on the grid of images a user has clicked
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
 
-    // TODO (2) Override onAttach to make sure that the container activity has implemented the callback
+    // Completed (2) Override onAttach to make sure that the container activity has implemented the callback
+    @Override
+    public void onAttach(Context context) {
 
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement OnImageClickListener");
+        }
+
+        super.onAttach(context);
+    }
 
     // Mandatory empty constructor
     public MasterListFragment() {
@@ -59,7 +76,13 @@ public class MasterListFragment extends Fragment {
         // Set the adapter on the GridView
         gridView.setAdapter(mAdapter);
 
-        // TODO (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        // Completed (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mCallback.onImageSelected(i);
+            }
+        });
 
         // Return the root view
         return rootView;
